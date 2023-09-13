29c63a2b1dbe3338f274bd108ddd4b138b919629
José Francisco Ruvalcaba Castañeda <32653773+frantizek@users.noreply.github.com>
2023-09-13 00:47:35 +0000

    clean up the code and avoid debug messages

--- generate_guild_users_list.py
+++ generate_guild_users_list.py
@@ -1,7 +1,8 @@
 import re
+import csv
 import requests
 from bs4 import BeautifulSoup
-import csv
+
 
 def read_file(filename):
     with open(filename, "r", encoding="utf-8") as f:
@@ -10,22 +11,17 @@ def read_file(filename):
 
 def search_telegram_user(user_name, dict_of_users):
     if user_name in dict_of_users.keys():
-        print("match")
         return "https://t.me/{}".format(dict_of_users[user_name].replace("@", ""))
     else:
         return " "
 
 def main():
-    users = []
     d_users = {}
     filename = "IM_AROA_YR.txt"
     lines = read_file(filename)
     for line in lines:
         user_data = line.strip().split(" ", 1)
-        users.append(user_data)
         d_users[user_data[1].lower()] = user_data[0].lower()
-
-    print(users)
 
     csv_header = ['Usuario swgoh', 'Codigo aliado', 'comando kryat', 'Usuario Telegram' ]
 
@@ -51,7 +47,6 @@ def main():
 
         for result in results:
             if "Name" in result.keys():
-                # print(result['Name'])
                 tag_converted_to_str = str(result['Name'])
                 username_swgoh = re.search(r'<div><strong>(.*?)</strong></div>', tag_converted_to_str).group(1)
                 allycode_swgoh = re.search(r'<a href="/p/(.*?)/">', tag_converted_to_str).group(1)
