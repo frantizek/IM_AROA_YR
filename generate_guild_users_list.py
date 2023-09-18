171bab51b2640c622f58926454958168987d5735
José Francisco Ruvalcaba Castañeda <32653773+frantizek@users.noreply.github.com>
2023-09-18 16:48:38 +0000

    Adding the images directory

--- generate_guild_users_list.py
+++ generate_guild_users_list.py
@@ -45,6 +45,12 @@ def main():
 
         table = soup.find('table')
 
+        acode_l = []
+        uswgoh_l = []
+        telegram_l = []
+        message_me_l = []
+        kryat_l = []
+
         headers = [header.text for header in table.find_all('th')]
         results = [{headers[i]: cell for i, cell in enumerate(row.find_all('td'))}
                    for row in table.find_all('tr')]
@@ -57,12 +63,51 @@ def main():
                 allycode_swgoh = re.search(r'<a href="/p/(.*?)/">', tag_converted_to_str).group(1)
                 kryat = f"/krayt max allycode: {allycode_swgoh}"
                 telegram = search_telegram_user(username_swgoh.lower(), d_users)
+                
+                acode_l.append(allycode_swgoh)
+                uswgoh_l.append(username_swgoh)
+                telegram_l.append(telegram)
+                message_me_l.append(f"https://t.me/{telegram}")
+                kryat_l.append(kryat)
 
                 # write the data
                 writer.writerow([allycode_swgoh, username_swgoh, telegram, kryat])
 
+
+
     # close the file
     this_file.close()
+
+    import plotly.graph_objects as go
+
+    headerColor = 'grey'
+    rowEvenColor = 'lightgrey'
+    rowOddColor = 'white'
+
+    fig = go.Figure(data=[go.Table(
+    header=dict(
+        values=['<b>Ally Code</b>','<b>Username SWGOH</b>','<b>Telegram User</b>','<b>Send Telegram Message</b>','<b>Kryat command</b>'],
+        line_color='darkslategray',
+        fill_color=headerColor,
+        align=['left','center'],
+        font=dict(color='white', size=12)
+    ),
+    cells=dict(
+        values=[
+        acode_l,
+        uswgoh_l,
+        telegram_l,
+        message_me_l,
+        kryat_l],
+        line_color='darkslategray',
+        # 2-D list of colors for alternating rows
+        fill_color = [[rowOddColor,rowEvenColor,rowOddColor, rowEvenColor]*50],
+        align = ['left', 'center'],
+        font = dict(color = 'darkslategray', size = 11)
+        ))
+    ])
+
+    fig.show()
 
 
 if __name__ == "__main__":
